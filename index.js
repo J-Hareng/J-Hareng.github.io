45c6ccbd62ad0f5c87208fbe6d8f56be45c0041f
Hakiraa <julian.hareng@gmail.com>
2023-01-30 14:31:13 +0100

    ad

--- index.js
+++ index.js
@@ -2,10 +2,29 @@
 //Changes the size and rotation of firstCard element when clicked by the user.
 const body = document.querySelector("body") 
 const firstCard = document.getElementById('firstcard');
+const secondCard = document.getElementById("secondcard");
+const thirdCard = document.getElementById("thirdcard");
+const fourthCard = document.getElementById("fourthcard");
+
+let isactive = false
 firstCard.addEventListener("click", () => {
-    firstCard.style.width = "98vmin";
-    firstCard.style.height = "98vh";
-    firstCard.style.transform = "none";
-    firstCard.style.transform = "rotate(0deg) translateX(-50%)";
-    firstCard.classList.remove("inactive")
+
+    if (isactive){
+    isactive = false
+    firstCard.classList.remove("active");
+    body.style.display = "grid";
+    } else {
+    isactive = true
+    
+    firstCard.classList.add("active");
+    body.style.display = "block";
+    }
+
+    
+    // firstCard.style.width = "98%";
+    // firstCard.style.height = "96%";
+
+    // firstCard.style.margin = "1%";
+    // firstCard.style.transform = "rotate(0deg) translateX(0)";
+    // firstCard.classList.remove("inactive")
 })
